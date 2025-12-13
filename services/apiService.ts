4b3a40bfb115b2ba039e7040355de12b35bf4613
Aakash Yadav <122480089+AakashYadv@users.noreply.github.com>
2025-12-13 23:57:53 +0530

    Delete services directory

--- services/apiService.ts
+++
@@ -1,8 +0,0 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
-
-export const sweetService = {
-  async getSweets() {
-    const res = await fetch(`${API_BASE}/api/sweets`);
-    return res.json();
-  }
-};
