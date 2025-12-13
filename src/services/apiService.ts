c487b6cc233664e677359421e61d77dcd21e9480
Aakash Yadav <122480089+AakashYadv@users.noreply.github.com>
2025-12-14 00:19:33 +0530

    Update apiService.ts

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,13 +2,6 @@ import { RegisterData, Sweet, User, Address, PaymentDetails, OrderStatus } from 
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
-export const sweetService = {
-  async getSweets() {
-    const res = await fetch(`${API_BASE}/api/sweets`);
-    return res.json();
-  }
-};
-
 
 /* ---------- AUTH ---------- */
 
